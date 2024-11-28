a66447f40ae4ffa2151d23e72b3b522766ff5107
EmmaWrightson <165636851+EmmaWrightson@users.noreply.github.com>
2024-11-27 17:32:07 -0800

    Added main.py
    added main.py file that will call the other .py files to more easily run the code

---
+++ main.py
@@ -0,0 +1,12 @@
+############################################################################
+# This is the main file for running the modular .py files.
+# Author: Xinshuang Liu, Haoyu Hu, Emma, Wrightson
+# Email: xil235@ucsd.edu, hah034@ucsd.edu, ewrightson@ucsd.edu
+############################################################################
+
+from dataset import TreeCoverLossDataset, DriverTypeDataset
+import numpy as np
+import math
+import warnings
+
+warnings.filterwarnings("ignore")
